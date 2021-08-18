f5b016bad2cc789d4a881b23f625edbe8613cdd1
ShiraiChisato <chst130726@gmail.com>
2021-08-18 13:32:04 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   get 'view/show'
   get 'task/show'
   get 'mypage/show'
+  resources :Users
+  resources :logins
+  resources :Projects
+  resources :Associates
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
