36745d3659e51093e3c17df30e069b5343abfaac
ShiraiChisato <chst130726@gmail.com>
2021-08-19 15:39:44 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :logins, controllers: {
-    session: 'logins/sessions',
-    registrations: 'logins/registrations'
-    }
   root 'mypage#show'
   get 'manage/show'
   get 'view/show'
   get 'task/show'
   get 'mypage/show'
-  resources :Users
+  devise_for :logins, controllers: {
+    session: 'logins/sessions',
+  }
   resources :logins
-  resources :Projects
-  resources :Associates
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
