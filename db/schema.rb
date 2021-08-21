56b0bbd2cb28be0a499b8807c4f35f0d1aff94b9
ShiraiChisato <chst130726@gmail.com>
2021-08-21 12:30:45 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_035829) do
+ActiveRecord::Schema.define(version: 2021_08_19_081608) do
 
   create_table "associates", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "userid"
@@ -20,18 +20,6 @@ ActiveRecord::Schema.define(version: 2021_08_17_035829) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "logins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_logins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_logins_on_reset_password_token", unique: true
-  end
-
   create_table "projects", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "projectname"
     t.datetime "created_at", precision: 6, null: false
@@ -39,11 +27,15 @@ ActiveRecord::Schema.define(version: 2021_08_17_035829) do
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
