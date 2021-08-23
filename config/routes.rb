8b36dedfbf3afd5e68256c72c202e3cc0a8542d1
ShiraiChisato <chst130726@gmail.com>
2021-08-23 13:28:28 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
   get 'view/show'
   get 'task/show'
   get 'mypage/show'
-  get 'users/show'
 
   resources :projects
   resources :associates
   resources :posts
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
