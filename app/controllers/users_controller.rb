c1eb197eb6e872e5e143d5e4a7560354b4561190
ShiraiChisato <chst130726@gmail.com>
2021-08-23 11:27:25 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
-
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
   private
     #ストロングパラメーター
     def user_params
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
   # minimagickを使うため
   include CarrierWave::MiniMagick
 
