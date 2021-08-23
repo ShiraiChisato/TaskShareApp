7c382500b6c2e987b77f2e8bc2f7d8377519fbe5
ShiraiChisato <chst130726@gmail.com>
2021-08-23 13:45:07 +0900

    update

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,13 +4,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # minimagickを使うため
   include CarrierWave::MiniMagick
 
-  #サムネイルの為に画像をリサイズ
-  version :thumb do
-    process resize_to_fit: [200, 200]
-  end
-  version :thumb50 do
-    process resize_to_fit: [100, 100]
-  end
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -22,12 +15,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
+  def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
@@ -37,15 +30,19 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
-
+  #サムネイルの為に画像をリサイズ
+  version :thumb do
+    process resize_to_fit: [200, 200]
+  end
+  version :thumb50 do
+    process resize_to_fit: [100, 100]
+  end
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+  # アップロードできる拡張子を制限
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
