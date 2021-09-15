d27f6d4f112a39a309a7ee2edc5ca6f473d7e792
ShiraiChisato <chst130726@gmail.com>
2021-09-15 21:32:23 +0900

    update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,33 +1,33 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
@@ -51,12 +51,23 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    after_sign_in_path_for(resource)
+  end
+
+  protected
+  def after_sign_in_path_for(resource)
+    redirect_to "mypage/show"
+  end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    super(resource)
+  end
+  private
+
+  #edit
+  def user_params
+      params.require(:user).permit(:icon, :name, :email, :encrypted_password )
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,74 +1,9 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
   def show
     if user_signed_in?
       @user = User.find(params[:id])
     else
       redirect_to new_user_session_path
     end
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      # update メソッドが正しく実行できたとき詳細ページに戻る
-      redirect_to @user
-      flash[:notice] = "正常にアップデートされました"
-    else
-      # updateできなかったときにエラーを表示
-      flash[:alert] = "エラー：#{@user.errors.full_messages}"
-      # renderで入力した内容はそのままで変種ページに戻る
-      render 'edit'
-    end
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to @user
-    else
-      error_mes = ""
-      @user.errors.full_messages.each do |error|
-        error_mes += "!#{error}<br>"
-      end
-      flash[:alert] = error_mes.html_safe
-      render :new
-    end
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    if @user.delete
-      flash[:notice] = "正常に削除されました"
-      redirect_to users_path
-    else
-      flash[:alert] = "削除できませんでした"
-      render :show
-    end
-  end
-
-  protected
-    def after_sign_up_path_for(resource)
-      after_sign_in_path_for(resource)
-  end
-
-  protected
-    def after_sign_in_path_for(resource)
-      "mypage/show"
-    end
-  private
-    def user_params
-      params.require(:user).permit(:icon, :name, :email, :encrypted_password )
   end
 end
