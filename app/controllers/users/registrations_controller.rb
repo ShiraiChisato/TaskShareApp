3ea6f0fca20397b4712499f8c364550ab06cddb2
ShiraiChisato <chst130726@gmail.com>
2021-09-17 15:09:46 +0900

    update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,7 +12,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
     super
-    redirect_to "users/show"
   end
 
   # GET /resource/edit
@@ -43,16 +42,29 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :icon])
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+
   end
 
   # The path used after sign up for inactive accounts.
   def after_inactive_sign_up_path_for(resource)
+    super(resource)
+  end
+  # The path used after sign in for inactive accounts.
+  def after_inactive_sign_in_path_for
     super(resource)
   end
   private
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,7 +11,6 @@ class Users::SessionsController < Devise::SessionsController
   #POST /resource/sign_in
   def create
     super
-    redirect_to "users/show"
   end
 
   #DELETE /resource/sign_out
