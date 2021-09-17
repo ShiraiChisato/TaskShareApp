31431be168f48ef3e3791f904be2c737dda5b9c2
ShiraiChisato <chst130726@gmail.com>
2021-09-17 14:18:21 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   resources :projects
   resources :associates
   resources :posts
-  resources :users, only: :show
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
