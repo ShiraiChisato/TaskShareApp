9009ddda82fa0d379432d4e9f5443384cda73f80
ShiraiChisato <chst130726@gmail.com>
2021-09-18 22:39:33 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     if user_signed_in?
       @user = current_user
-      @project = Associate.find(current_user.id)
     else
       redirect_to new_user_session_path
     end
