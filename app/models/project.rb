1601ebd97aad1a3cf9839f3e0322d649edff84ac
ShiraiChisato <chst130726@gmail.com>
2021-09-21 22:09:57 +0900

    update

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,10 @@
 class Project < ApplicationRecord
-  mount_uploader :icon, ImageUploader
-
+  
   has_many :host_users_associate, -> {where(host: :true)},class_name: 'Associate'
   has_many :host_users, through: :host_users_associate, source: :user
   has_many :associates
   has_many :users, through: :associates
   has_many :posts
+
+  mount_uploader :icon, ImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
-  has_many :projects, through: :associates
+  
   has_many :associates
+  has_many :projects, through: :associates
   has_many :posts
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
