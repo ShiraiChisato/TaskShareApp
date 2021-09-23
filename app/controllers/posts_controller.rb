17a52524c18be2e606d3f6394bc97e2fa9dd7e08
ShiraiChisato <chst130726@gmail.com>
2021-09-23 11:07:13 +0900

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,16 +13,17 @@ class PostsController < ApplicationController
   end
 
   def create
+    @project = Project.find(params[:post][:project_id])
     @post =  current_user.posts.new(posts_params)
     if current_user.save
-      redirect_to project_path(@post.project)
+      redirect_to project_path(@project)
     else
       error_mes = ""
       @post.errors.full_messages.each do |error|
         error_mes += "!#{error}<br>"
       end
       flash[:alert] = error_mes.html_safe
-      redirect_to new_post_path(project_id: @post.project)
+      redirect_to new_post_path(project_id: @project)
     end
   end
 
@@ -36,6 +37,6 @@ class PostsController < ApplicationController
 
   private
     def posts_params
-      params.require(:post).permit(:post1, :post2, :post3, :post4, :post5)
+      params.require(:post).permit(:post1, :post2, :post3, :post4, :post5).merge(project: @project)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  validates :post1,  presence: true
 end
